e8bb61ce5c132aa5cb04c2344eb140d8d57edb9e
R <ryansereno@protonmail.com>
2022-09-06 18:44:55 -0400

    corrected .gitignore, added useEffect to context

--- src/store/cart-provider.js
+++ src/store/cart-provider.js
@@ -1,16 +1,44 @@
 import CartContext from './cart-context'
+import React, {useReducer} from 'react'
+
+const defaultCartState = {
+    items:[],
+    totalAmount:0,
+}
+
+const cartReducer = (state, action) =>{
+    if(action.type === 'ADD-ITEM'){
+        const updatedItems = state.items.concat(action.item)
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    if(action.type === 'REMOVE-ITEM')
+    return defaultCartState
+}
+
 
 const CartProvider = props =>{
-    const addItemToCartHandler = (item)=>{
+const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
+    const addItemToCartHandler = (item)=>{
+        dispatchCartAction({
+            type:'ADD-ITEM',
+            item:item
+        })
     }
     const removeItemFromCartHandler = (id)=>{
-
+        dispatchCartAction({
+            type:'REMOVE-ITEM',
+            id:id
+        })
     }
 
     const cartContext ={
-        items:[],
-        totalAmount:0,
+        items:cartState.items,
+        totalAmount:cartState.totalAmount,
         additem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     }
