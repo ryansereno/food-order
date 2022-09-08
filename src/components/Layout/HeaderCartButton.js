0d1ec327c86973d36f89e75978b50488a23a3efe
R <ryansereno@protonmail.com>
2022-09-08 14:33:05 -0400

    added cart button animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,34 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
-import CartIcon from '../Cart/CartIcon'
-import cartContext from '../../store/cart-context'
+import CartIcon from "../Cart/CartIcon";
+import cartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-const cartCtx = useContext(cartContext)
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) =>{
-        return currentNumber + item.amount
-    },0)
+  const cartCtx = useContext(cartContext);
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+    const {items} = cartCtx
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+    useEffect(() =>{
+        if (items.length === 0){
+            return
+        }
+       setBtnIsHighlighted(true) 
+        const timer = setTimeout(()=>{
+            setBtnIsHighlighted(false)
+        }, 300)
+
+        return()=>{
+            clearTimeout(timer)
+        }
+    },[items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
-        <CartIcon/>
+        <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
