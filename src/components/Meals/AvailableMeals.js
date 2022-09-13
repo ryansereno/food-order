2226923f7e14e8de42ef54b16b2525c60044c290
R <ryansereno@protonmail.com>
2022-09-13 17:27:52 -0400

    added http request hook, and connected to firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import useHttp from "../../hooks/use-http";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [meals, setMeals] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp();
+
+  useEffect(() => {
+    const transformMealsData = (mealsObject) => {
+      const loadedMeals = [];
+      for (const mealKey in mealsObject) {
+        //this transformation is specific to firebase
+        loadedMeals.push({
+          id: mealKey,
+          description: mealsObject[mealKey].description,
+          name: mealsObject[mealKey].name,
+          price: mealsObject[mealKey].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals(
+      {
+        url: "https://react-test-fd9c9-default-rtdb.firebaseio.com/meals.json",
+      },
+      transformMealsData
+    );
+  }, [fetchMeals]);
+
+  const mealsList = meals.map((meal) => {
     return (
       <MealItem
-          id = {meal.id}
+        id={meal.id}
         key={meal.id}
         name={meal.name}
         description={meal.description}
@@ -42,6 +42,21 @@ const AvailableMeals = () => {
       />
     );
   });
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+          <p>Loading...</p>
+      </section>
+    );
+  }
+  if (error) {
+    return (
+      <section className={classes.MealsError}>
+          <p>Something went wrong</p>
+      </section>
+    );
+  }
   return (
     <section className={classes.meals}>
       <Card>
